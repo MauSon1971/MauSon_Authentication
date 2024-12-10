28edc3fd2e96f051b94b8165cf250b6c95c3bd59
MauSon1971 <110358138+MauSon1971@users.noreply.github.com>
2024-12-10 21:18:36 +0000

    01_ Plantilla Conectada

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash #(Libreria que sirve para guardar una constraseña segura)
 
 api = Blueprint('api', __name__)
 
@@ -18,5 +20,50 @@ def handle_hello():
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
+
+
+@api.routes.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return jsonify({"message": "El email y password son requeridos"}), 400
+
+    # Verifica si el usuario ya existe
+    if User.query.filter_by(email=email).first():
+        return jsonify({"message": "El usuario ya existe"}), 400
+
+    # Crea el usuario con contraseña en hash
+    hashed_password = generate_password_hash(password)
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "Usuario registrado exitosamente"}), 201
+
+@api.route ('/login', methods=['POST'])
+def handle_Login():
+    # Verificar si los datos están presentes
+    data = request.json
+    if not data:
+        return jsonify({"message": "Debe enviar datos en el cuerpo de la solicitud"}), 400
+
+    # Obtener email y contraseña del cuerpo de la solicitud
+    email = data.get('email')
+    password = data.get('password')
+
+    # Validar email y contraseña
+    if not email or not password:
+        return jsonify({"message": "El email y password son requeridos"}), 400
+
+    # Buscar el usuario por email en la base de datos
+    user = User.query.filter_by(email=email, password=password).first()
+    if not user:
+        return jsonify({"message": "Usuario o contraseña incorrectos"}), 401
+
+    # Crear un token de acceso utilizando el email del usuario
+    token = create_access_token(identity=user.email)
+    return(token)
